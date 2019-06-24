3e8f2f81d6a811dd8e8720864b111d5b2825fde3
BlackMG27 <oike279@gmail.com>
2019-06-24 12:52:06 -0500

    added the barebones functionality to the app

---
+++ assets/js/tip.js
@@ -0,0 +1,52 @@
+//set up the variables
+const percent = document.getElementById('percentage');
+const totalTip = document.getElementById('totalTip');
+const tipPerPerson = document.getElementById('tipPerPerson');
+const totalAmount = document.getElementById('totalAmount');
+const amountPerson = document.getElementById('amountPerson')
+let tipPercent;
+//get the tipPercent
+function getPercent() {
+    tipPercent = percent.options[percent.selectedIndex].value;
+}
+//sets up the calculation
+document
+    .getElementById('tipForm')
+    .addEventListener('submit', function (e) {
+        e.preventDefault();
+        getPercent();
+        const people = getValue('people');
+        const price = getValue('price');
+        //sends the three values to the functions
+        totalTips(price, tipPercent);
+        totalPrice(price, tipPercent);
+        totalPerson(people, price, tipPercent);
+        amountPerPerson(people, price);
+
+    });
+
+function getValue(id) {
+    return document
+        .getElementById(id)
+        .value;
+}
+
+function totalTips(price, percent) {
+    const result = price * percent;
+    totalTip.textContent = result.toFixed(2);
+}
+
+function totalPrice(price, percent) {
+    const totalPrice = parseInt(price) + Number(price * percent);
+    totalAmount.textContent = totalPrice.toFixed(2);
+}
+
+function totalPerson(people, price, percent) {
+    const tipPerson = Number(price * percent) / Number(people);
+    tipPerPerson.textContent = tipPerson.toFixed(2);
+}
+
+function amountPerPerson(people, price) {
+    const finalAmount = Number(price / people);
+    amountPerson.textContent = finalAmount.toFixed(2);
+}
